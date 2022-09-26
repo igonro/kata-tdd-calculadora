a4a91f90c8e1af714c05ac8d12d382c8a635cbf6
Iago <6860957+igonro@users.noreply.github.com>
2022-09-26 14:48:42 +0200

    finish task

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,11 @@
+OP_TO_FUNC = {
+    "suma": "suma",
+    "resta": "resta",
+    "mult": "multiplicacion",
+    "div": "division",
+}
+
+
 def suma(a, b):
     return a + b
 
@@ -14,4 +22,45 @@ def division(a, b):
     try:
         return a / b
     except ZeroDivisionError:
-        return "Error: No se puede dividir entre cero"
+        raise ValueError("Error: No se puede dividir entre cero")
+
+
+def parse_entrada(texto):
+    args = texto.strip().split(" ")
+    if len(args) != 3:
+        raise ValueError("Error: Número de parámetros incorrecto")
+    op, n1, n2 = args
+    if op not in OP_TO_FUNC:
+        raise ValueError("Error: Introduce una operación válida")
+    try:
+        n1, n2 = float(n1), float(n2)
+    except ValueError:
+        raise ValueError("Error: Introduce un número válido")
+    return OP_TO_FUNC[op], n1, n2
+
+
+def ejecutar_operacion(operacion, n1, n2):
+    func = globals()[operacion]
+    return func(n1, n2)
+
+
+def main():
+    print("¡Hola! Introduce una operación")
+    while True:
+        try:
+            entrada = input("> ")
+            if entrada == "q":
+                break
+            try:
+                op, n1, n2 = parse_entrada(entrada)
+                resultado = ejecutar_operacion(op, n1, n2)
+                print(f"Resultado: {resultado}")
+            except ValueError as e:
+                print(e)
+        except KeyboardInterrupt:
+            break
+    print("¡Adiós!")
+
+
+if __name__ == "__main__":
+    main()
--- test_calculadora.py
+++ test_calculadora.py
@@ -1,39 +1,67 @@
+import pytest
 from numpy.testing import assert_almost_equal
 
 import calculadora as calc
 
 
-def test_funcion_suma():
-    assert calc.suma(2, 2) == 4
+@pytest.mark.parametrize("n1,n2,expected", [(2, 2, 4), (2.5, -6, -3.5)])
+def test_suma_devuelve_resultado_correcto(n1, n2, expected):
+    assert calc.suma(n1, n2) == expected
 
 
-def test_funcion_suma_numeros_negativos_y_decimales():
-    assert calc.suma(2.5, -6) == -3.5
+@pytest.mark.parametrize("n1,n2,expected", [(3, 2, 1), (3.3, -1.2, 4.5)])
+def test_resta_devuelve_resultado_correcto(n1, n2, expected):
+    assert calc.resta(n1, n2) == expected
 
 
-def test_funcion_resta():
-    assert calc.resta(3, 2) == 1
+@pytest.mark.parametrize("n1,n2,expected", [(5, 3, 15), (5.1, -3, -15.3)])
+def test_multiplicacion_devuelve_resultado_correcto(n1, n2, expected):
+    assert_almost_equal(calc.multiplicacion(n1, n2), expected)
 
 
-def test_funcion_resta_numeros_negativos_y_decimales():
-    assert calc.resta(3.3, -1.2) == 4.5
+@pytest.mark.parametrize("n1,n2,expected", [(5, 2, 2.5), (5.5, -1.1, -5)])
+def test_division_devuelve_resultado_correcto(n1, n2, expected):
+    assert calc.division(n1, n2) == expected
 
 
-def test_funcion_multiplicacion():
-    assert calc.multiplicacion(5, 3) == 15
+def test_division_entre_0_devuelve_error():
+    with pytest.raises(ValueError, match="Error: No se puede dividir entre cero"):
+        calc.division(5, 0)
 
 
-def test_funcion_multiplicacion_numeros_negativos_y_decimales():
-    assert_almost_equal(calc.multiplicacion(5.1, -3), -15.3)
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        ("suma 2 4.4", ("suma", 2, 4.4)),
+        ("div -3.1 0", ("division", -3.1, 0)),
+        ("mult -3.33 1.0", ("multiplicacion", -3.33, 1.0)),
+    ],
+)
+def test_parsear_texto_devuelve_resultado_correcto(input, expected):
+    assert calc.parse_entrada(input) == expected
 
 
-def test_funcion_division():
-    assert calc.division(5, 2) == 2.5
+@pytest.mark.parametrize(
+    "input,exception",
+    [
+        ("suma 2 4.4 5", "Error: Número de parámetros incorrecto"),
+        ("suma", "Error: Número de parámetros incorrecto"),
+        ("asdf 2 4.4", "Error: Introduce una operación válida"),
+        ("suma 2 qwer", "Error: Introduce un número válido"),
+    ],
+)
+def test_parsear_entrada_devuelve_error_con_argumentos_incorrectos(input, exception):
+    with pytest.raises(ValueError, match=exception):
+        calc.parse_entrada(input)
 
 
-def test_funcion_division_numeros_negativos_y_decimales():
-    assert calc.division(5.5, -1.1) == -5
-
-
-def test_funcion_division_entre_0_devuelve_error():
-    assert calc.division(5, 0) == "Error: No se puede dividir entre cero"
+@pytest.mark.parametrize(
+    "op,n1,n2,expected",
+    [
+        ("suma", 2, 4.4, 6.4),
+        ("division", -3.1, 2, -1.55),
+        ("multiplicacion", -3.33, 10, -33.3),
+    ],
+)
+def test_ejecutar_operacion_devuelve_resultado_correcto(op, n1, n2, expected):
+    assert calc.ejecutar_operacion(op, n1, n2) == expected
